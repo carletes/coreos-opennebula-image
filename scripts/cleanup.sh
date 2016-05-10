131f723278caafdd374240dd27c97f1c9d61d24b
Dominic Luechinger <dol@cyon.ch>
2016-05-10 16:11:10 +0200

    Reformating of shell scripts
    1.) Replaced tabs with spaces
    2.) Replaced direct call of bash with /usr/bin/env

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Remove SSH server keys from image.
-
+# Keys will be regenerated on first boot => sshd-keygen.service
 sudo rm -f /etc/ssh/ssh_host_*_key
 sudo rm -f /etc/ssh/ssh_host_*_key.pub
